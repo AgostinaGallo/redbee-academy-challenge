d1be6d7c92643d627c4a3ea84661bc679379f5e4
Agostina Gallo <agostinamgallo@gmail.com>
2021-06-15 21:46:22 -0300

    added test: CompareArrayNumbers

--- src/test/java/com/redbee/academy/challenge/CompareArrayNumbersTests.java
+++ src/test/java/com/redbee/academy/challenge/CompareArrayNumbersTests.java
@@ -4,9 +4,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CompareArrayNumbersTests {
 
@@ -38,5 +44,25 @@ public class CompareArrayNumbersTests {
     assertEquals(5, result.get(0));
     assertEquals(5, result.get(1));
     assertEquals(1, result.get(2));
+  }
+
+/**
+* Added tests for challenge
+**/
+  @Test
+  @DisplayName("Tests RedBee challenge")
+  public void testRedBeeChallenge() {
+
+    List<Integer> list1 = Arrays.asList(10, 8, 1, 3);
+    List<Integer> list2 = Arrays.asList(4, 2, 0, 10);
+
+    List<Integer> maxListResult = Arrays.asList(Collections.max(list1), Collections.max(list2));
+
+    assertNotNull(maxListResult, "The result cannot be null");
+
+    assertThat(maxListResult, contains(10,10));
+
+    assertEquals(10, maxListResult.get(0));
+    assertEquals(10, maxListResult.get(1));
   }
 }
